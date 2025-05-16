38a31a5b55c9926a89fca79e32b7c34b61528b7a
meylaniic <meylaniic@gmail.com>
2025-05-16 08:49:33 +0700

    first commit

---
+++ queuemain.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+
+public class queuemain {
+
+    public static void menu() {
+        System.out.println("Masukkan operasi yang diinginkan : ");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("6. Keluar");
+        System.out.println("---------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas queue: ");
+        int n = sc.nextInt();
+
+        queue q = new queue(n);
+        int pilih;
+
+        do {
+            menu();
+            pilih = sc.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru : ");
+                    int datamasuk = sc.nextInt();
+                    q.enqueue(datamasuk);
+                    break;
+                case 2:
+                    int datakeluar = q.dequeue();
+                    if (datakeluar != 0) {
+                        System.out.print("Data yang dikeluarkan: " + datakeluar);
+                    }
+                    break;
+                case 3:
+                    q.print();
+                    break;
+                case 4:
+                    q.peek();
+                    break;
+                case 5:
+                    q.clear();
+                    break;
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+    }
+}
